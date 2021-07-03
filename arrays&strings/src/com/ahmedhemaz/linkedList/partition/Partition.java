10d1a1e4b0198295c23952bf5f1f150305d9912a
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-07-03 22:50:52 +0200

    test bug when partition is the head node

--- arrays&strings/src/com/ahmedhemaz/linkedList/partition/Partition.java
+++ arrays&strings/src/com/ahmedhemaz/linkedList/partition/Partition.java
@@ -5,20 +5,21 @@ import com.ahmedhemaz.linkedList.LinkedListImp.LinkedList;
 public class Partition {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
-        linkedList.add(3);
-        linkedList.add(5);
-        linkedList.add(8);
-        linkedList.add(5);
-        linkedList.add(10);
         linkedList.add(2);
         linkedList.add(1);
+        linkedList.add(4);
+        linkedList.add(3);
+        linkedList.add(2);
+        linkedList.add(5);
+        linkedList.add(2);
         linkedList.printLinkedList();
         System.out.println();
-        partition(linkedList, 5);
+        partition(linkedList, 2);
         linkedList.printLinkedList();
 
     }
 
+    // bug when the partiation is the head node
     public static void partition(LinkedList ls, int partitionVal) {
         LinkedList.Node firstLowerNode = ls.getHead();
         LinkedList.Node current = ls.getHead();
@@ -30,6 +31,7 @@ public class Partition {
             }else {
                 current = current.next;
             }
+            System.out.println(current.value);
         }
         if (ls.getHead().value >= partitionVal) {
             current.next = new LinkedList.Node(ls.getHead().value);
